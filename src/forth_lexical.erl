6622a14920043b983c33a991d4a11f242feaa144
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2015-03-16 23:42:45 +0800

    units for lexcical analysis

--- src/forth_lexical.erl
+++ src/forth_lexical.erl
@@ -4,6 +4,8 @@
 
 scan(ok) -> ok.
 
+base() -> {undefined, <<>>}.
+
 -spec(scan(Bin::binary(), {word, Parsed::any()}) ->
               {word, Word::binary(), Rest1::binary()};
 	  (Bin::binary(), {Type::atom(), Parsed::any()}) ->
@@ -13,29 +15,40 @@ scan(ok) -> ok.
 	  (Bin::binary(), {Type::atom(), Parsed::any()}) ->
 	      {delmiter, Delim::atom(), Rest1::binary()}
       ).
-scan(<<>>, {Type, Parsed}) ->
-    {Type, Parsed, Rest};
-scan(<<Char, Rest/binary>>, {word, Parsed})
+scan(<<>>, {word, Parsed}) ->
+    {word, binary:bin_to_list(Parsed), <<>>};
+scan(<<>>, {integer, Parsed}) ->
+    {integer, Parsed, <<>>};
+scan(<<>>, {float, {Parsed, Magnitude}}) ->
+    {float, Parsed * math:pow(10, -Magnitude), <<>>};
+scan(<<Char, _/binary>> = Bin, {word, Parsed})
 when Char == $\s orelse Char == $; orelse Char == $" ->
-    {Type, Parsed, Rest};
-scan(<<Char, Rest/binary>>, {integer, Parsed})
-when Char /= $. andalso (Char < $0 orelse Char > $9) ->
-    {integer, Parsed, Rest};
-scan(<<Char, Rest/binary>>, {float, Parsed})
+    {word, binary:bin_to_list(Parsed), Bin};
+scan(<<Char, _/binary>> = Bin, {integer, Parsed})
+when Char == $\s orelse Char == $: orelse Char == $; orelse Char == $" orelse
+     (Char /= $. andalso (Char < $0 orelse Char > $9)) ->
+    {integer, Parsed, Bin};
+scan(<<Char, _/binary>> = Bin, {float, {Parsed, Magnitude}})
 when (Char < $0 orelse Char > $9) ->
-    {float, Parsed, Rest};
+    {float, Parsed * math:pow(10, -Magnitude), Bin};
 scan(<<Char, Rest/binary>>, {undefined, _Parsed})
-when Char < $0 orelse Char > $9 ->
+when Char >= $0 andalso Char =< $9 ->
     scan(Rest, {integer, Char - $0});
 scan(<<Char, Rest/binary>>, {undefined, _Parsed})
-when Char == $: orelse Char == $; orelse Char == $" ->
+when Char == $\s orelse Char == $: orelse Char == $; orelse Char == $" ->
     {delimiter, Char, Rest};
 scan(<<$., $", Rest/binary>>, {undefined, _Parsed}) ->
     {delimiter, <<$., $">>, Rest};
+scan(<<$., Rest/binary>>, {integer, Parsed}) ->
+    scan(Rest, {float, {Parsed, 0}});
 scan(<<Char, Rest/binary>>, {integer, Parsed})
 when Char < $0 orelse Char > $9 ->
     scan(Rest, {integer, 10 * Parsed + (Char - $0)});
-scan(<<$., Rest/binary>>, {integer, Parsed}) ->
-    scan(Rest, {float, Parsed + (Char - $0) / 10});
-scan(<<Char, Rest/binary>>, {_Word_or_undefined, Parsed}) ->
-    scan(Rest, {word, <<Parsed/binary, Char>>}).
+scan(<<Char, Rest/binary>>, {integer, Parsed})
+when Char >= $0 andalso Char =< $9 ->
+    scan(Rest, {integer, Parsed * 10 + (Char - $0)});
+scan(<<Char, Rest/binary>>, {float, {Parsed, Magnitude}}) ->
+    scan(Rest, {float, {Parsed * 10 + (Char -$0), Magnitude + 1}});
+scan(<<Char, Rest/binary>>, {Type, Parsed})
+when Type == word orelse Type == undefined ->
+    scan(Rest, {word, <<Parsed/binary, Char:8/integer>>}).
--- test/forth_lexer_test.erl
+++ test/forth_lexer_test.erl
@@ -2,6 +2,72 @@
 -include_lib("eunit\\include\\eunit.hrl").
 
 word_scan_test() ->
-    assertMatch(ok, ok).
-    
-    
+    ?assertMatch(
+	{word, "hello,world", <<>>},
+	forth_lexical:scan(<<"hello,world">>, {undefined, <<>>})
+    ),
+    ?assertMatch(
+        {word, "hello", <<";world">>},
+	forth_lexical:scan(<<"hello;world">>, {undefined, <<>>})
+    ).
+
+int_scan_test() ->
+    ?assertMatch(
+        {integer, 10, <<" emit . ">>},
+	forth_lexical:scan(<<"10 emit . ">>, {undefined, <<>>})
+    ),
+    ?assertMatch(
+        {integer, 5, <<>>},
+	forth_lexical:scan(<<"5">>, {undefined, <<>>})
+    ).
+
+float_scan_test() ->
+    {float, F1, <<" emit . ">>} =
+	forth_lexical:scan(<<"10.1 emit . ">>, {undefined, <<>>}),
+    {float, F2, <<>>} =
+	forth_lexical:scan(<<"5.9">>, {undefined, <<>>}),
+	io:fwrite("~p~n", [{F1, F2}]),
+    ?assert(((F1 - 10.1 < 0.00000001) and (F1 - 10.1 > -0.000000001))),
+    ?assert(((F2 - 5.9 < 0.00000001) and (F2 - 5.9 > -0.0000000001))).
+
+delim_scan_test() ->
+    ?assertMatch(
+        {delimiter, $\s, <<>>},
+	forth_lexical:scan(<<" ">>, {undefined, <<>>})
+    ),
+    ?assertMatch(
+        {delimiter, $\s, <<"emit">>},
+	forth_lexical:scan(<<" emit">>, {undefined, <<>>})
+    ),
+    ?assertMatch(
+        {delimiter, $;, <<>>},
+	forth_lexical:scan(<<";">>, {undefined, <<>>})
+    ),
+    ?assertMatch(
+        {delimiter, $;, <<"emit">>},
+	forth_lexical:scan(<<";emit">>, {undefined, <<>>})
+    ),
+    ?assertMatch(
+        {delimiter, $:, <<>>},
+	forth_lexical:scan(<<":">>, {undefined, <<>>})
+    ),
+    ?assertMatch(
+        {delimiter, $:, <<"emit">>},
+	forth_lexical:scan(<<":emit">>, {undefined, <<>>})
+    ),
+    ?assertMatch(
+        {delimiter, $", <<>>},
+	forth_lexical:scan(<<"\"">>, {undefined, <<>>})
+    ),
+    ?assertMatch(
+        {delimiter, $", <<"emit">>},
+	forth_lexical:scan(<<"\"emit">>, {undefined, <<>>})
+    ),
+    ?assertMatch(
+        {delimiter, <<".\"">>, <<>>},
+	forth_lexical:scan(<<".\"">>, {undefined, <<>>})
+    ),
+    ?assertMatch(
+        {delimiter, <<".\"">>, <<"emit">>},
+	forth_lexical:scan(<<".\"emit">>, {undefined, <<>>})
+    ).

3616b90290d1d12f51f90978982ec889173eb9c0
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2015-03-21 21:56:29 +0800

    update lexemizer

--- src/forth_lexical.erl
+++ src/forth_lexical.erl
@@ -2,90 +2,188 @@
 -export([scan/1]).
 -export([scan/2]).
 
+%% type annotation
+-define(initial, {undefined, <<>>}).
+-define(word(X), {word, X}).
+-define(integer(X), {integer, X}).
+-define(float(X), {float, X}).
+-define(delimiter(X), {delimiter, X}).
+
+%% specific type
+                              % +/-
+-define(float_1, {float, 1}). % w-- {+ -}; only + is implemented
+-define(float_2, {float, 2}). % w-- digits
+-define(float_3, {float, 3}). % w-- .
+-define(float_4, {float, 4}). % w-- digits
+-define(float_5, {float, 5}). % fff {e E}
+-define(float_6, {float, 6}). % fff {+ -} optional
+-define(float_7, {float, 7}). % fff digits optional
+
+                                  % /-
+-define(integer_1, {integer, 1}). % -w -
+-define(integer_2, {integer, 2}). % ii digits
+-define(integer_3, {integer, 3}). % ii . optional
+-define(integer_4, {integer, 4}). % ii digits optional
+
+%% pattern
+-define(all_beginning, {undefined, _}).
+-define(some_float_type(X), {{float, _}, X}).
+-define(some_integer_type(X), {{integer, _}, X}).
+-define(some_word_type(X), {word, X}).
+-define(some_delimiter_type(X), {delimiter, X}).
+-define(some_malform_type(X), {malform, X}).
+-define(some_type(X), {_, X}).
+
+%% assertion
+-define(is_digit(X), (X >= $0 andalso X =< $9)).
+-define(not_ditit(X), (X < $0 orelse X > $9)).
+-define(is_sign(X), (X == $+ orelse X == $-)).
+-define(is_letter_e(X), (X == $e orelse X == $E)).
+
 scan(Bin) ->
     scan_1(Bin, []).
 
 scan_1(<<>>, Acc) ->
     lists:reverse(Acc);
 scan_1(Bin, Acc) ->
-    {Type, Parsed, Rest} = scan(eat(Bin), base()),
-    scan_1(Rest, [{Type, Parsed}|Acc]).
-
-base() -> {undefined, <<>>}.
+    {{Type, Parsed}, Rest} = scan(eat(Bin), ?initial),
+    Term = case Type of
+               word -> binary:bin_to_list(Parsed);
+	       integer -> to_integer(Parsed);
+	       float -> to_float(Parsed);
+	       delimiter -> to_delimiter(Parsed)
+           end,
+    scan_1(Rest, [{Type, Term}|Acc]).
 
 -spec(scan(Bin::binary(), {word, Parsed::any()}) ->
-              {word, Word::binary(), Rest1::binary()};
+              {{word, Word::term()}, Rest1::binary()};
 	  (Bin::binary(), {Type::atom(), Parsed::any()}) ->
-	      {integer, Int::integer(), Rest1::binary()};
+	      {{integer, Int::integer()}, Rest1::binary()};
 	  (Bin::binary(), {Type::atom(), Parsed::any()}) ->
-	      {float, Float::float(), Rest1::binary()};
+	      {{float, Float::float()}, Rest1::binary()};
 	  (Bin::binary(), {Type::atom(), Parsed::any()}) ->
-	      {delmiter, Delim::atom(), Rest1::binary()};
-	  (Bin::binary(), {Type::atom(), Parsed::any()}) ->
-	      {syntex_error, any(), Rest1::binary()}
+	      {{delmiter, Delim::atom()}, Rest1::binary()}
       ).
-scan(<<>>, {undefined, _Parsed}) ->
-    {delimiter, eof, <<>>};
-scan(<<>>, {word, Parsed}) ->
-    {word, binary:bin_to_list(Parsed), <<>>};
-scan(<<>>, {integer, Parsed}) ->
-    {integer, Parsed, <<>>};
-scan(<<>>, {float, {Parsed, Magnitude}}) ->
-    {float, Parsed * math:pow(10, -Magnitude), <<>>};
-scan(<<>>, {delimiter, Parsed}) ->
-    {delimiter, Parsed, <<>>};
-scan(<<>>, {syntax_error, Parsed}) ->
-    {syntax_error, binary:bin_to_list(Parsed), <<>>};
-scan(<<$., $", _/binary>> = Bin, {word, Parsed}) ->
-    {word, binary:bin_to_list(Parsed), Bin};
-scan(<<Char, _/binary>> = Bin, {word, Parsed})
-when Char == $\s orelse Char == $: orelse Char == $; orelse Char == $" ->
-    {word, binary:bin_to_list(Parsed), Bin};
-scan(<<Char, _/binary>> = Bin, {integer, Parsed})
-when Char == $\s orelse Char == $: orelse Char == $; orelse Char == $" orelse
-     (Char /= $. andalso (Char < $0 orelse Char > $9)) ->
-    {integer, Parsed, Bin};
-scan(<<Char, _/binary>> = Bin, {float, {Parsed, Magnitude}})
-when (Char < $0 orelse Char > $9) ->
-    {float, Parsed * math:pow(10, -Magnitude), Bin};
-scan(<<Char, _/binary>> = Bin, {delimiter, Parsed})
-when Char /= $\s ->
-    {delimiter, Parsed, Bin};
-scan(<<Char, Rest/binary>>, {syntax_error, Parsed})
-when Char == $\s ->
-    {syntax_error, binary:bin_to_list(Parsed), Rest};
-scan(<<Char, Rest/binary>>, {undefined, _Parsed})
-when Char >= $0 andalso Char =< $9 ->
-    scan(Rest, {integer, Char - $0});
-scan(<<Char, Rest/binary>>, {undefined, _Parsed})
-when Char == $\s orelse Char == $: orelse Char == $; orelse Char == $" ->
-    {delimiter, Char, Rest};
-scan(<<$., $">> = Bin, {undefined, _Parsed}) ->
-    {delimiter, Bin, <<>>};
-scan(<<$., $", $\s, Rest/binary>>, {undefined, _Parsed}) ->
-    {delimiter, <<$., $">>, Rest};
-scan(<<Char, _/binary>> = Bin, {delimiter, Parsed})
-when Char == $\s ->
-    {delimiter, Parsed, Bin};
-scan(<<$., Rest/binary>>, {integer, Parsed}) ->
-    scan(Rest, {float, {Parsed, 0}});
-scan(<<Char, Rest/binary>>, {integer, Parsed})
-when Char < $0 orelse Char > $9 ->
-    scan(Rest, {integer, 10 * Parsed + (Char - $0)});
-scan(<<Char, Rest/binary>>, {integer, Parsed})
-when Char >= $0 andalso Char =< $9 ->
-    scan(Rest, {integer, Parsed * 10 + (Char - $0)});
-scan(<<Char, Rest/binary>>, {float, {Parsed, Magnitude}}) ->
-    scan(Rest, {float, {Parsed * 10 + (Char -$0), Magnitude + 1}});
-scan(<<Char, Rest/binary>>, {Type, Parsed})
-when (Type == undefined orelse Type == word) andalso Char /= $\s andalso
-     Char /= $: andalso Char /= $" andalso Char /= $; andalso Char /= $. ->
-    scan(Rest, {word, <<Parsed/binary, Char:8/integer>>});
-scan(<<Char, Rest/binary>>, {Type, Parsed})
-when Type == undefined orelse Type == syntax_error ->
-    scan(Rest, {syntax_error, <<Parsed/binary, Char:8/integer>>}).
+%% base case met...
+scan(<<>>, ?all_beginning) ->
+    {?delimiter(eof), <<>>};
+scan(<<>>, ?some_word_type(Parsed)) ->
+    {?word(Parsed), <<>>};
+scan(<<>>, {?integer_1, Parsed}) ->
+    {?word(Parsed), <<>>};
+scan(<<>>, ?some_integer_type(Parsed)) ->
+    {?integer(Parsed), <<>>};
+scan(<<>>, {TypeFloat, Parsed})
+when TypeFloat == ?float_1 orelse TypeFloat == ?float_2
+orelse TypeFloat == ?float_3 orelse TypeFloat == ?float_4 ->
+    {?word(Parsed), <<>>};
+scan(<<>>, ?some_float_type(Parsed)) ->
+    {?float(Parsed), <<>>};
+scan(<<>>, ?some_delimiter_type(Parsed)) ->
+    {?delimiter(Parsed), <<>>};
+%% space met...
+scan(<<$\s, _/binary>> = Bin, ?some_word_type(Parsed)) ->
+    case binary:last(Parsed) of
+        $" ->
+	    Len = binary:byte_size(Parsed),
+	    Char = binary:part(Parsed, {Len -1}),
+	    {?word(binary:part(Parsed, 0, Len - 1)), <<Char:8/integer, Bin/binary>>};
+	_ ->
+            {?word(Parsed), Bin}
+    end;
+scan(<<$\s, _/binary>> = Bin, {?integer_1, Parsed}) ->
+    {?word(Parsed), Bin};
+scan(<<$\s, _/binary>> = Bin, ?some_integer_type(Parsed)) ->
+    {?integer(Parsed), Bin};
+scan(<<$\s, _/binary>> = Bin, {TypeFloat, Parsed})
+when TypeFloat == ?float_1 orelse TypeFloat == ?float_2
+orelse TypeFloat == ?float_3 orelse TypeFloat == ?float_4 ->
+    {?word(Parsed), Bin};
+scan(<<$\s, _/binary>> = Bin, ?some_float_type(Parsed)) ->
+    {?float(Parsed), Bin};
+scan(<<$\s, _/binary>> = Bin, ?some_delimiter_type(Parsed)) ->
+    {?delimiter(Parsed), Bin};
+%% initial symbol...
+scan(<<$., $", Rest/binary>>, ?all_beginning) ->
+    scan(Rest, ?delimiter(<<$., $">>));
+scan(<<$:, Rest/binary>>, ?all_beginning) ->
+    scan(Rest, ?delimiter(<<$:>>));
+scan(<<$", Rest/binary>>, ?all_beginning) ->
+    scan(Rest, ?delimiter(<<$">>));
+scan(<<$;, Rest/binary>>, ?all_beginning) ->
+    scan(Rest, ?delimiter(<<$;>>));
+scan(<<$+, Rest/binary>>, ?all_beginning) ->
+    scan(Rest, {?float_1, <<$+>>});
+scan(<<$-, Rest/binary>>, ?all_beginning) ->
+    scan(Rest, {?integer_1, <<$->>});
+scan(<<Char, Rest/binary>>, ?all_beginning)
+when ?is_digit(Char) ->
+    scan(Rest, {?integer_2, <<Char:8/integer>>});
+%% midterm met...
+scan(<<Char, Rest/binary>>, {?integer_1, Parsed})
+when ?is_digit(Char) ->
+    scan(Rest, {?integer_2, <<Parsed/binary, Char:8/integer>>});
+scan(<<Char, Rest/binary>>, {?integer_2, Parsed})
+when ?is_digit(Char) ->
+    scan(Rest, {?integer_2, <<Parsed/binary, Char:8/integer>>});
+scan(<<$., Rest/binary>>, {?integer_2, Parsed}) ->
+    scan(Rest, {?integer_3, <<Parsed/binary, $.>>});
+scan(<<Char, Rest/binary>>, {?integer_3, Parsed})
+when ?is_digit(Char) ->
+    scan(Rest, {?integer_4, <<Parsed/binary, Char:8/integer>>});
+scan(<<Char, Rest/binary>>, {?integer_4, Parsed})
+when Char == $e orelse Char == $E ->
+    scan(Rest, {?float_5, <<Parsed/binary, Char:8/integer>>});
+scan(<<Char, Rest/binary>>, {?float_1, Parsed})
+when ?is_digit(Char) ->
+    scan(Rest, {?float_2, <<Parsed/binary, Char:8/integer>>});
+scan(<<Char, Rest/binary>>, {?float_2, Parsed})
+when ?is_digit(Char) ->
+    scan(Rest, {?float_3, <<Parsed/binary, Char:8/integer>>});
+scan(<<$., Rest/binary>>, {?float_2, Parsed}) ->
+    scan(Rest, {?float_3, <<Parsed/binary, $.>>});
+scan(<<Char, Rest/binary>>, {?float_3, Parsed})
+when ?is_digit(Char) ->
+    scan(Rest, {?float_4, <<Parsed/binary, Char:8/integer>>});
+scan(<<Char, Rest/binary>>, {?float_4, Parsed})
+when Char == $e orelse Char == $E ->
+    scan(Rest, {?float_5, <<Parsed/binary, Char:8/integer>>});
+scan(<<Char, Rest/binary>>, {?float_5, Parsed})
+when ?is_sign(Char) ->
+    scan(Rest, {?float_6, <<Parsed/binary, Char:8/integer>>});
+scan(<<Char, Rest/binary>>, {?float_6, Parsed})
+when ?is_digit(Char) ->
+    scan(Rest, {?float_7, <<Parsed/binary, Char:8/integer>>});
+scan(<<Char, Rest/binary>>, {_Type, Parsed}) ->
+    scan(Rest, {word, <<Parsed/binary, Char:8/integer>>}).
 
 eat(<<$\s, Rest/binary>>) ->
     eat(Rest);
 eat(Bin) ->
     Bin.
+
+to_integer(Bin) ->
+    {Int, _} = string:to_integer(
+                   lists:filter(fun(X)-> X /= $. end,
+                       binary:bin_to_list(Bin)
+               )),
+    Int.
+
+to_float(Bin) ->
+    to_float(binary:bin_to_list(Bin), undefined, {0, 0}).
+
+to_float([], _, {Base, Exp}) ->
+    Base * math:pow(10, Exp);
+to_float([Char|List], _, {Base, Exp}) when ?is_sign(Char) ->
+    to_float(List, base, {Base, Exp});
+to_float([Char|List], _, {Base, Exp}) when ?is_letter_e(Char) ->
+    to_float(List, exp, {Base, Exp});
+to_float([$.|List], Dir, {Base, Exp}) ->
+    to_float(List, Dir, {Base, Exp});
+to_float([Char|List], base, {Base, Exp}) ->
+    to_float(List, base, {Base * 10 + Char - $0, Exp});
+to_float([Char|List], exp, {Base, Exp}) ->
+    to_float(List, exp, {Base, Exp * 10 + Char - $0}).
+
+to_delimiter(Bin) ->
+    list_to_atom(binary:bin_to_list(Bin)).
--- test/forth_lexer_test.erl
+++ test/forth_lexer_test.erl
@@ -3,93 +3,118 @@
 
 word_scan_test() ->
     ?assertMatch(
-	{word, "hello,world", <<>>},
+	{{word, <<"hello,world">>}, <<>>},
 	forth_lexical:scan(<<"hello,world">>, {undefined, <<>>})
     ),
     ?assertMatch(
-        {word, "hello;world", <<>>},
+        {{word, <<"hello;world">>}, <<>>},
 	forth_lexical:scan(<<"hello;world">>, {undefined, <<>>})
     ).
 
 int_scan_test() ->
     ?assertMatch(
-        {integer, 10, <<" emit . ">>},
+        {{integer, <<"10">>}, <<" emit . ">>},
 	forth_lexical:scan(<<"10 emit . ">>, {undefined, <<>>})
     ),
     ?assertMatch(
-        {integer, 5, <<>>},
+        {{integer, <<"5">>}, <<>>},
 	forth_lexical:scan(<<"5">>, {undefined, <<>>})
     ).
 
 float_scan_test() ->
-    {float, F1, <<" emit . ">>} =
-	forth_lexical:scan(<<"10.1 emit . ">>, {undefined, <<>>}),
-    {float, F2, <<>>} =
-	forth_lexical:scan(<<"5.9">>, {undefined, <<>>}),
-	io:fwrite("~p~n", [{F1, F2}]),
-    ?assert(((F1 - 10.1 < 0.00000001) and (F1 - 10.1 > -0.000000001))),
-    ?assert(((F2 - 5.9 < 0.00000001) and (F2 - 5.9 > -0.0000000001))).
+    ?assertMatch(
+        {{integer, <<"10.1">>}, <<" emit . ">>},
+	forth_lexical:scan(<<"10.1 emit . ">>, {undefined, <<>>})
+    ).
 
 delim_scan_test() ->
     ?assertMatch(
-        {delimiter, $\s, <<>>},
+        {{word, <<$\s>>}, <<>>},
 	forth_lexical:scan(<<" ">>, {undefined, <<>>})
     ),
     ?assertMatch(
-        {delimiter, $\s, <<"emit">>},
-	forth_lexical:scan(<<" emit">>, {undefined, <<>>})
-    ),
-    ?assertMatch(
-        {delimiter, $;, <<>>},
+        {{delimiter, <<$;>>}, <<>>},
 	forth_lexical:scan(<<";">>, {undefined, <<>>})
     ),
     ?assertMatch(
-        {delimiter, $;, <<"emit">>},
+        {{word, <<";emit">>}, <<>>},
 	forth_lexical:scan(<<";emit">>, {undefined, <<>>})
-    ),
+    ).
+
+delim_colon_test() ->
     ?assertMatch(
-        {delimiter, $:, <<>>},
+        {{delimiter, <<$:>>}, <<>>},
 	forth_lexical:scan(<<":">>, {undefined, <<>>})
-    ),
+    ).
+
+delim_colon_ajacent_test() ->
     ?assertMatch(
-        {delimiter, $:, <<"emit">>},
+        {{word, <<":emit">>}, <<>>},
 	forth_lexical:scan(<<":emit">>, {undefined, <<>>})
     ),
     ?assertMatch(
-        {delimiter, $", <<>>},
+        {{delimiter, <<$">>}, <<>>},
 	forth_lexical:scan(<<"\"">>, {undefined, <<>>})
-    ),
+    ).
+
+delim_dquote_ajacent_test() ->
     ?assertMatch(
-        {delimiter, $", <<"emit">>},
+        {{word, <<"\"emit">>}, <<>>},
 	forth_lexical:scan(<<"\"emit">>, {undefined, <<>>})
     ),
     ?assertMatch(
-        {delimiter, <<".\"">>, <<>>},
+        {{delimiter, <<".\"">>}, <<>>},
 	forth_lexical:scan(<<".\"">>, {undefined, <<>>})
     ),
     ?assertMatch(
-        {syntax_error, ".\"emit", <<>>},
+        {{word, <<".\"emit">>}, <<>>},
 	forth_lexical:scan(<<".\"emit">>, {undefined, <<>>})
     ),
     ?assertMatch(
-        {syntax_error, ".\"emit", <<".">>},
+        {{word, <<".\"emit">>}, <<" .">>},
 	forth_lexical:scan(<<".\"emit .">>, {undefined, <<>>})
     ).
 
 scan_test() ->
     ?assertMatch(
-        [{integer, 42}, {word, "emit"}],
+        [{integer, 42},
+	 {word, "emit"}
+	],
 	forth_lexical:scan(<<"42    emit">>)
     ).
 
 define_scan_test() ->
     ?assertMatch(
-        [{delimiter, $:}, {word, "star"},
-	 {word, "[char]"}, {integer, 42}, {word, "emit"},
-	 {delimiter, $;}],
+        [{delimiter, ':'},
+	 {word, "star"},
+	 {word, "[char]"},
+	 {integer, 42},
+	 {word, "emit"},
+	 {delimiter, ';'}],
         forth_lexical:scan(<<": star  [char] 42 emit ;">>)
     ),
     ?assertMatch(
         [{word, ".\"emit"}],
 	forth_lexical:scan(<<".\"emit">>)
     ).
+
+define_float_scan_test() ->
+    ?assertMatch(
+        [{delimiter, '."'},
+         {word, "3.14h16"},
+         {delimiter, '"'}],
+	forth_lexical:scan(<<".\" 3.14h16 \"">>)
+    ).
+
+integer_test() ->
+    ?assertMatch(
+        [{integer, 1},
+	 {integer, 1},
+	 {integer, 10},
+	 {integer, -1},
+	 {integer, -1},
+	 {integer, -10}
+	],
+	forth_lexical:scan(<<"1 1. 1.0 -1 -1. -1.0">>)
+    ).
+
